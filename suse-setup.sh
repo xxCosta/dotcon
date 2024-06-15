9b98359fcf4099a8f8a5b06669a8d6c7b4f3253d
xxCosta <104746249+xxCosta@users.noreply.github.com>
2024-06-15 19:15:28 -0300

    init suse-setup.sh
    for vms running leap15.6> or  tumbleweed

---
+++ suse-setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo zypper install -y docker git neovim
+sudo zypper install -y  gcc clang  zig npm
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+git clone https://github.com/xxCosta/nvim.git ~/.config/nvim
+wget https://raw.githubusercontent.com/xxCosta/dotcon/main/.bashrc
+source .bashrc
