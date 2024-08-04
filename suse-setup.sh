d387143ff602870f461242a5f2290a72b9ad2216
xxCosta <104746249+xxCosta@users.noreply.github.com>
2024-08-04 13:15:10 -0300

    Update suse-setup.sh
    added tmux config to startup downloads

--- suse-setup.sh
+++ suse-setup.sh
@@ -2,4 +2,5 @@
 sudo zypper install -y docker docker-compose git neovim gcc clang  zig npm tmux
 git clone https://github.com/xxCosta/nvim.git ~/.config/nvim
 wget https://raw.githubusercontent.com/xxCosta/dotcon/main/.bashrc -P ~
+https://raw.githubusercontent.com/xxCosta/dotcon/main/.tmux.conf -P ~
 source ~/.bashrc
