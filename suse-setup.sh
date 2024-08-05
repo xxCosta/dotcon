fe62ab285a4563adab938326f8f6f2b5f4c13a55
xxCosta <104746249+xxCosta@users.noreply.github.com>
2024-08-05 18:28:40 -0300

    Update suse-setup.sh
    added easy ssh setup script

--- suse-setup.sh
+++ suse-setup.sh
@@ -2,5 +2,6 @@
 sudo zypper install -y docker docker-compose git neovim gcc clang  zig npm tmux
 git clone https://github.com/xxCosta/nvim.git ~/.config/nvim
 wget https://raw.githubusercontent.com/xxCosta/dotcon/main/.bashrc -P ~
-https://raw.githubusercontent.com/xxCosta/dotcon/main/.tmux.conf -P ~
+wget https://raw.githubusercontent.com/xxCosta/dotcon/main/.tmux.conf -P ~
+wget https://raw.githubusercontent.com/xxCosta/dotcon/main/ssh-setup.sh
 source ~/.bashrc
