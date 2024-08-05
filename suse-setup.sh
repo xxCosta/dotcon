076bc7fc1bdac27bae69befd9203347218cd3404
xxCosta <104746249+xxCosta@users.noreply.github.com>
2024-08-05 18:32:10 -0300

    Update suse-setup.sh
    added update to start of script, added bun installation

--- suse-setup.sh
+++ suse-setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+sudo zypper update -y
 sudo zypper install -y docker docker-compose git neovim gcc clang  zig npm tmux
+sudo npm i -g bun
 git clone https://github.com/xxCosta/nvim.git ~/.config/nvim
 wget https://raw.githubusercontent.com/xxCosta/dotcon/main/.bashrc -P ~
 wget https://raw.githubusercontent.com/xxCosta/dotcon/main/.tmux.conf -P ~
