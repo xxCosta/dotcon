cd558021d9549b16b7088967d6d78d401e1ead41
xxCosta <104746249+xxCosta@users.noreply.github.com>
2024-08-05 18:53:10 -0300

    Update suse-setup.sh
    added b to npm install

--- suse-setup.sh
+++ suse-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo zypper update -y
 sudo zypper install -y docker docker-compose git neovim gcc clang  zig npm tmux
-sudo npm i -g bun
+sudo npm i -g bun n
 git clone https://github.com/xxCosta/nvim.git ~/.config/nvim
 wget https://raw.githubusercontent.com/xxCosta/dotcon/main/.bashrc -P ~
 wget https://raw.githubusercontent.com/xxCosta/dotcon/main/.tmux.conf -P ~
