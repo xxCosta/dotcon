442104655f5e92255ed7fe97a57297df7dc48a3b
xxCosta <104746249+xxCosta@users.noreply.github.com>
2024-08-05 18:29:18 -0300

    Update suse-setup.sh
    fixed download location

--- suse-setup.sh
+++ suse-setup.sh
@@ -3,5 +3,5 @@ sudo zypper install -y docker docker-compose git neovim gcc clang  zig npm tmux
 git clone https://github.com/xxCosta/nvim.git ~/.config/nvim
 wget https://raw.githubusercontent.com/xxCosta/dotcon/main/.bashrc -P ~
 wget https://raw.githubusercontent.com/xxCosta/dotcon/main/.tmux.conf -P ~
-wget https://raw.githubusercontent.com/xxCosta/dotcon/main/ssh-setup.sh
+wget https://raw.githubusercontent.com/xxCosta/dotcon/main/ssh-setup.sh -P ~
 source ~/.bashrc
