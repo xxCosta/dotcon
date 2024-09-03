184639b157eec318b7bddebb8b4da335e27c2145
xxCosta <104746249+xxCosta@users.noreply.github.com>
2024-09-03 19:31:44 -0300

    Update suse-setup.sh
    download tmux plugin manager

--- suse-setup.sh
+++ suse-setup.sh
@@ -3,6 +3,7 @@ sudo zypper update -y
 sudo zypper install -y docker docker-compose git neovim gcc clang  zig npm tmux
 sudo npm i -g bun n
 git clone https://github.com/xxCosta/nvim.git ~/.config/nvim
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 wget https://raw.githubusercontent.com/xxCosta/dotcon/main/.bashrc -P ~
 wget https://raw.githubusercontent.com/xxCosta/dotcon/main/.tmux.conf -P ~
 wget https://raw.githubusercontent.com/xxCosta/dotcon/main/ssh-setup.sh -P ~
